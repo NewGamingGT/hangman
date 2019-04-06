0b7b6dfee97183319c57bc72f2fd09a9c1078dbf
NewGaming <39025657+NewGamingGT@users.noreply.github.com>
2019-04-06 14:55:49 +0800

    Update randomword.py

--- randomword.py
+++ randomword.py
@@ -13,12 +13,9 @@ for _ in range(len(randomWord)):
 # Add starting characters
 characters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-probability = 10
+probability = 20
 for _ in range(probability):
 	randomCharacters = random.choice(characters)
 	for index, Value in enumerate(randomWord):
 		if Value == randomCharacters:
 			array[index] = Value
-
-# Add first character as starting character
-array[0] = randomWord[0]
